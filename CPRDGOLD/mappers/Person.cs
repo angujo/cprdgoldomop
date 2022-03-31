f55909a0c5139bd09996d6fed744736d33fccb14
angujo <angujomondi@gmail.com>
2022-03-31 09:46:57 +0100

    Commit person to binary

--- CPRDGOLD/mappers/Person.cs
+++ CPRDGOLD/mappers/Person.cs
@@ -29,11 +29,8 @@ namespace CPRDGOLD.mappers
 
         protected override void LoadData()
         {
-            // PropertyInfo[] props = typeof(Person).GetProperties();
             string[] cols = new string[] { "person_id", "gender_concept_id", "year_of_birth", "month_of_birth", "race_concept_id", "ethnicity_concept_id", "care_site_id",
                 "person_source_value", "gender_source_value", "gender_source_concept_id", "race_source_concept_id", "ethnicity_source_concept_id" };
-
-            // Log.Info(string.Join("\t", props.Select(pi => pi.Name)));
             DB.Target.CopyBinaryRows("person", cols, (row, write) =>
             {
 
@@ -52,57 +49,6 @@ namespace CPRDGOLD.mappers
                           write(0);
                           write(0);
                           write(0);
-
-                          #region Works
-                          /*  DB.Target.CopyText("person",cols,writer => {
-                              writer.Write(patient.patid);
-                              writer.Write(Delimiter);
-                              writer.Write(1 == patient.gender ? 8507 : 8532);
-                              writer.Write(Delimiter);
-                              writer.Write(patient.yob.ToString().Length < 4 ? patient.yob + 1800 : patient.yob);
-                              writer.Write(Delimiter);
-                              writer.Write(patient.mob);
-                              writer.Write(Delimiter);
-                              writer.Write(0);
-                              writer.Write(Delimiter);
-                              writer.Write(0);
-                              writer.Write(Delimiter);
-                              writer.Write(patient.care_site_id);
-                              writer.Write(Delimiter);
-                              writer.Write(patient.patid);
-                              writer.Write(Delimiter);
-                              writer.Write(patient.gender);
-                              writer.Write(Delimiter);
-                              writer.Write(0);
-                              writer.Write(Delimiter);
-                              writer.Write(0);
-                              writer.Write(Delimiter);
-                              writer.Write(0);
-                              writer.WriteLine();
-                          });
-                        Person p = new Person
-                          {
-                              person_id = patient.patid,
-                              gender_concept_id = 1 == patient.gender ? 8507 : 8532,
-                              year_of_birth = patient.yob.ToString().Length < 4 ? patient.yob + 1800 : patient.yob,
-                              month_of_birth = patient.mob,
-                              race_concept_id = 0,
-                              ethnicity_concept_id = 0,
-                              care_site_id = patient.care_site_id,
-                              person_source_value = patient.patid,
-                              gender_source_value = patient.gender,
-                              gender_source_concept_id = 0,
-                              race_source_concept_id = 0,
-                              ethnicity_source_concept_id = 0,
-                          };
-                          DB.Target.CopyInsert<Person>(writer =>
-                          {
-                              var row = string.Join("\t", props.Select(pi => pi.GetValue(p, null)));
-                              Log.Info(row);
-                              writer.WriteLine(row);
-                          });*/
-                          //  Add();
-                          #endregion
                       });
             });
         }
